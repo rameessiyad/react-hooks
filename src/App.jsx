5cffe8ddced066b0cf914d0b67a28b9d705e2dae
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-12-26 22:04:08 +0530

    count state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
     </>
   );
 }
